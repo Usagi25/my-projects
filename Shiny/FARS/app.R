b6faf35b71d3d8ad1fbc3dbe7156f82fe03f2065
Usagi25 <ajnz1234@gmail.com>
2022-07-20 08:01:47 -0400

    Made the "Other" category and layout changed

--- Shiny/FARS/app.R
+++ Shiny/FARS/app.R
@@ -1,15 +1,13 @@
 # A new Shiny App to help me with data visualization on the FARS data
-# The version as it is does a poor job of displaying the data because the plot
-# is not large enough to show all the x labels, and overall there are often
-# too many x labels. Also, in the case of the HOURS plot, I want 99 to come
-# immediately after 24 so as not to leave a giant gap.
+# Things to do: consolidate any categories beyond 7 into "other", except for
+# HOURS and MONTHNAME.
 
 library(tidyverse)
 library(shiny)
 library(ggplot2)
 
 # Load data ----
-crash_data <- read.csv("../accident.csv")
+crash_data <- read.csv("accident.csv")
 
 # Clean data ----
 cr.df <- crash_data %>%
@@ -20,53 +18,63 @@ cr.df <- crash_data %>%
          "RELJCT2NAME", "TYP_INTNAME", "REL_ROADNAME", "LGT_CONDNAME",
          "WEATHERNAME", "FATALS", "DRUNK_DR")
 
+cr.df[, !names(cr.df) %in% c("MONTH", "DAY_WEEK")] <- 
+  data.frame(lapply(cr.df[, !names(cr.df) %in% c("MONTH", "DAY_WEEK")], as.factor))
+
 cr.df$MONTHNAME <- reorder(cr.df$MONTHNAME, cr.df$MONTH)
 cr.df$DAY_WEEKNAME <- reorder(cr.df$DAY_WEEKNAME, cr.df$DAY_WEEK)
 
 ui <- fluidPage(
-  sidebarLayout(
-    
-    sidebarPanel(
-      
-      selectInput("selection",
-                  label = h3("Variable to display"),
-                  choices = list("State" = "STATENAME",
-                                 "Pedestrians involved" = "PEDS",
-                                 "Month" = "MONTHNAME",
-                                 "Day of the week" = "DAY_WEEKNAME",
-                                 "Hour" = "HOUR",
-                                 "Route Type" = "ROUTENAME",
-                                 "Rural/Urban" = "RUR_URBNAME",
-                                 "Functional class" = "FUNC_SYSNAME",
-                                 "Harmful event" = "HARM_EVNAME",
-                                 "RELJCT2" = "RELJCT2NAME",
-                                 "Intersection" = "TYP_INTNAME",
-                                 "Relation to trafficway" = "REL_ROADNAME",
-                                 "Light conditions" = "LGT_CONDNAME",
-                                 "Weather" = "WEATHERNAME",
-                                 "Fatalities" = "FATALS",
-                                 "Drunk drivers" = "DRUNK_DR"),
-                  selected = "PEDS")
-      
-    ),
-    
-    mainPanel(
-      
-      plotOutput("barplot") # Got an error when I forgot the quote );
-      
-    )
-    
-  )
+  fluidRow(
+    column(4,
+           selectInput("selection",
+                       label = h3("Variable to display"),
+                       choices = list("State" = "STATENAME",
+                                      "Pedestrians involved" = "PEDS",
+                                      "Month" = "MONTHNAME",
+                                      "Day of the week" = "DAY_WEEKNAME",
+                                      "Hour" = "HOUR",
+                                      "Route Type" = "ROUTENAME",
+                                      "Rural/Urban" = "RUR_URBNAME",
+                                      "Functional class" = "FUNC_SYSNAME",
+                                      "Harmful event" = "HARM_EVNAME",
+                                      "RELJCT2" = "RELJCT2NAME",
+                                      "Intersection" = "TYP_INTNAME",
+                                      "Relation to trafficway" = "REL_ROADNAME",
+                                      "Light conditions" = "LGT_CONDNAME",
+                                      "Weather" = "WEATHERNAME",
+                                      "Fatalities" = "FATALS",
+                                      "Drunk drivers" = "DRUNK_DR"),
+                       selected = "PEDS")
+           ),
+    column(4,
+           offset = 4,
+           tableOutput("table"))
+  ),
+  fluidRow(
+    column(12,
+           plotOutput("barplot"))
+  ),
 )
+
+count_top <- function(df, var, n = 7) {
+  df %>%
+    mutate({{ var }} := fct_lump_n({{ var }}, n = n)) %>%
+    count({{ var }}, sort = TRUE)
+}
 
 server <- function(input, output) {
   
-  dataSelection <- reactive(cr.df %>% count(!!sym(input$selection)))
+  dataSelection <- reactive(count_top(cr.df, !!sym(input$selection)))
+  
+  output$table <- renderTable(
+    dataSelection()
+  )
   
   output$barplot <- renderPlot({
     dataSelection() %>%
       ggplot(aes_string(x = input$selection, "n"))+
-      geom_col()
+      geom_col(fill = "blue")
   })
   
 }
